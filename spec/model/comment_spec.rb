571040197e6832857df011bc28a93c7453fa1924
ekenecf <ekens2u@gmail.com>
2022-07-23 14:41:28 +0100

    Fix linters

--- spec/model/comment_spec.rb
+++ spec/model/comment_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe Comment, type: :model do
   subject { Comment.new(text: 'I love programming') }
   before { subject.save }
 
-  it 'No post raise error' do
+  it 'No comment raise error' do
     expect { subject.commentcounter }.to raise_error(NoMethodError)
   end
-
 end
