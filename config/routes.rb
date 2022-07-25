f63d3e98ccc41748c8c7731c53ae446dec085990
ekenecf <ekens2u@gmail.com>
2022-07-25 18:50:29 +0100

    Add show files

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'comments/index'
-  get 'likes/index'
   get 'posts/index'
   root to: 'users#index'
 
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index new ] do
-      resources :likes, only: %i[new ]
-    resources :comments, only: %i[new ]
-    end
+  resources :users, only: %i[index new show] do
+    resources :posts, only: %i[index new show]
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
