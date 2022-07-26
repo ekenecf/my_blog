824ead8a6d49a771308b5bd3e0854161496d6c34
ekenecf <ekens2u@gmail.com>
2022-07-26 16:14:08 +0100

    Create controller spec for user

---
+++ spec/controller_spec/user_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    before(:example) { get('/users') }
+
+    it 'should have an http success response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders correct page contents' do
+      expect(response.body).to include('Here is a list of users')
+    end
+
+    it 'renders index template' do
+      expect(response).to render_template('index')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:example) { get('/users/20') }
+    it 'should have an http success response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders correct page contents' do
+      expect(response.body).to include('Specific user')
+    end
+
+    it 'renders index template' do
+      expect(response).to render_template('show')
+    end
+  end
+end
