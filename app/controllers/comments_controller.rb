e5dbe62e4be17d30f795f518848c0eb8a3d8baaa
ekenecf <ekens2u@gmail.com>
2022-07-29 19:05:25 +0100

    Create comment methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   def index
+    @post = Post.find(params[:user_id])
     @comments = Comment.all
   end
 
@@ -9,5 +10,29 @@ class CommentsController < ApplicationController
     @comment = Comment.new
   end
 
-  
+  def create
+    comment = params[:comment]
+    user = User.find(params[:user_id])
+    post = Post.find(params[:post_id])
+    puts user, post.comments_counter
+    created_comment = Comment.new(comment.permit(:text))
+    created_comment.author_id = user.id
+    created_comment.post_id = post.id
+    if created_comment.save
+      redirect_to user_post_url(user, post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
