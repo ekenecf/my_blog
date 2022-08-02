ec09f23997aad605f5a28507ad6c134349551b2d
ekenecf <ekens2u@gmail.com>
2022-08-02 18:21:33 +0100

    Add comments for clarity

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -12,7 +12,8 @@ class LikesController < ApplicationController
   def create
     @post = Post.includes(:user).find(params[:post_id])
     @post_author = @post.user
-
+    
+    # current_user is gotten from ApplicationController
     @the_user = current_user
 
     @liked_posts_by_this_user = Like.where(user: @the_user, post: @post)
