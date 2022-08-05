0c46082b250869cc1a0b0f8ea667c178ed04fcb5
ekenecf <ekens2u@gmail.com>
2022-08-05 22:16:30 +0100

    Fix linters

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ require_relative '../config/environment'
 
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
-require "capybara/rspec"
+require 'capybara/rspec'
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -14,11 +14,9 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.use_transactional_fixtures = true
-
 
   config.infer_spec_type_from_file_location!
 
@@ -31,7 +29,7 @@ RSpec.configure do |config|
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
-  
+
   config.before(:each, js: true) do
     DatabaseCleaner.strategy = :truncation
   end
