3d63cd32d37efb604e9137f4d3e4b99e132a6f67
ekenecf <ekens2u@gmail.com>
2022-08-10 21:14:04 +0100

    Fix rubocop offenses

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,15 +2,11 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new
+    user ||= User.new # support guest user ie users not logged in
     can :read, Post, public: true
+
     return unless user.present? # additional permissions for logged in users (they can read their own posts)
-
     can :read, User # all users can view users
-    can :read, Post # all users can view posts
-    can :read, Comment # all users can view comments
-    return unless user.present?
-
     can :manage, Post, user: user # only post owners can manage posts
     can :manage, Comment, user: user # only post owners can manage posts
     return unless user.admin? # additional permissions for administrators
